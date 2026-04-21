d03575fc8d590a787dc992323af507e5672048c4
TwinTails <twintail.f@gmail.com>
2026-04-21 10:35:08 +0700

    fix publisher 1

--- autodoc/publisher/page_manager/hierarchy_manager.py
+++ autodoc/publisher/page_manager/hierarchy_manager.py
@@ -3,7 +3,15 @@
 from autodoc.infrastructure.logger import logger
 from autodoc.publisher.clients.protocols import IConfluenceClient
 
-_COMPONENT_PAGE_BODY: str = "Автоматически созданная страница компонента"
+_COMPONENT_PAGE_BODY: str = (
+    "Автоматически созданная страница компонента"
+    '<p>'
+    '<ac:structured-macro ac:macro-id="8cb4ae85-0212-4b3d-a15f-77899af1f1d7" '
+    'ac:name="children" ac:schema-version="2">'
+    '<ac:parameter ac:name="depth">3</ac:parameter>'
+    '</ac:structured-macro>'
+    '</p>'
+)
 _VERSION_PAGE_BODY: str = "Автоматически созданная страница версии"
 
 
